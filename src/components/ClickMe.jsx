453cd42ae4b4dcd41735b9c168e0dc9b2b4ed688
eiramevi <angelajlamera@gmail.com>
2025-02-21 16:02:47 +0800

    to-do-list

---
+++ src/components/ClickMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const ClickMe = () => {
+  return (
+    <div>
+      <Button>Click ME</Button>
+    </div>
+  );
+};
+
+export default ClickMe;
